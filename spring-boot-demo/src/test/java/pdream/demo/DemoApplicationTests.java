34cd79c3d06c71e198e5d67990b7a716901ee77a
dongjianzhu <27027915@qq.com>
2020-02-17 18:57:35 +0800

    测试

--- spring-boot-demo/src/test/java/pdream/demo/DemoApplicationTests.java
+++ spring-boot-demo/src/test/java/pdream/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package pdream.demo;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import javax.sql.DataSource;
+
+@SpringBootTest(classes = DemoApplication.class)
+@RunWith(SpringRunner.class)
 class DemoApplicationTests {
+
+    @Autowired
+    private  DataSource dataSource;
 
     @Test
     void contextLoads() {
+
+        HikariDataSource hikariDataSource = (HikariDataSource) dataSource;
+        int maximumPoolSize = hikariDataSource.getMaximumPoolSize();
+        System.out.println(maximumPoolSize);
     }
 
 }
